cfdfe707e8ea49ae89cb24d28a9edcda267397ca
dslyvka <daniilslyvka@gmail.com>
2022-06-04 15:29:57 +0300

    fixed tag p

--- src/pages/registerPage/registerPage.jsx
+++ src/pages/registerPage/registerPage.jsx
@@ -1,32 +1,7 @@
 import { Formik } from 'formik';
-import * as yup from 'yup';
+import validationSchema from '../../validation/register.js';
 
 const LoginPage = () => {
-  const validationSchema = yup.object().shape({
-    name: yup
-      .string()
-      .typeError('Має бути рядком')
-      .required("Це поле обов'язкове"),
-    email: yup
-      .string()
-      .matches(
-        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-        'Email має бути валідним',
-      )
-      .required("Це поле обов'язкове"),
-    password: yup
-      .string()
-      .required("Це поле обов'язкове")
-      .min(
-        6,
-        'Занадто короткий пароль. Пароль має містити мінімум 6 символів.',
-      ),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref('password')], 'Паролі мають співпадати')
-      .required("Це поле обов'язкове"),
-  });
-
   return (
     <Formik
       initialValues={{
