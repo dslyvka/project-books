24bc057f420a7cf532412577b5700271e22fca76
dslyvka <daniilslyvka@gmail.com>
2022-06-04 14:33:35 +0300

    updated packages

--- src/pages/loginPage/loginPage.jsx
+++ src/pages/loginPage/loginPage.jsx
@@ -28,32 +28,31 @@ const LoginPage = () => {
   });
 
   return (
-    <>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          password: '',
-          confirmPassword: '',
-        }}
-        validateOnBlur
-        validationSchema={validationSchema}
-        onSubmit={values => console.log(values)}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          isValid,
-          dirty,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-        }) => (
-          <div>
-            <button type="button">Google</button>
-            <form>
-              {/* <p>
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: '',
+      }}
+      validateOnBlur
+      validationSchema={validationSchema}
+      onSubmit={values => console.log(values)}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        isValid,
+        dirty,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      }) => (
+        <div>
+          <button type="button">Google</button>
+          <form>
+            {/* <p>
               {!values.name.length || errors.name ? (
                 <label htmlFor="name">Ім'я *</label>
               ) : (
@@ -73,47 +72,47 @@ const LoginPage = () => {
               {touched.name && errors.name && <span>{errors.name}</span>}
             </p> */}
 
-              <p>
-                {!values.email.length || errors.email ? (
-                  <label htmlFor="email">Електронна адреса *</label>
-                ) : (
-                  <label htmlFor="email">Електронна адреса</label>
-                )}
-                <br />
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="your@email.com"
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.email}
-                />
-                <br />
-                {touched.email && errors.email && <span>{errors.email}</span>}
-              </p>
+            <p>
+              {!values.email.length || errors.email ? (
+                <label htmlFor="email">Електронна адреса *</label>
+              ) : (
+                <label htmlFor="email">Електронна адреса</label>
+              )}
+              <br />
+              <input
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+              <br />
+              {touched.email && errors.email && <span>{errors.email}</span>}
+            </p>
 
-              <p>
-                {!values.password.length || errors.password ? (
-                  <label htmlFor="password">Пароль *</label>
-                ) : (
-                  <label htmlFor="password">Пароль</label>
-                )}
-                <br />
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="..."
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  value={values.password}
-                />
-                <br />
-                {touched.password && errors.password && (
-                  <span>{errors.password}</span>
-                )}
-              </p>
+            <p>
+              {!values.password.length || errors.password ? (
+                <label htmlFor="password">Пароль *</label>
+              ) : (
+                <label htmlFor="password">Пароль</label>
+              )}
+              <br />
+              <input
+                type="password"
+                name="password"
+                placeholder="..."
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+              <br />
+              {touched.password && errors.password && (
+                <span>{errors.password}</span>
+              )}
+            </p>
 
-              {/* <p>
+            {/* <p>
               {!values.confirmPassword.length || errors.confirmPassword ? (
                 <label htmlFor="confirmPassword">Підтвердити пароль *</label>
               ) : (
@@ -134,32 +133,26 @@ const LoginPage = () => {
               )}
             </p> */}
 
-              <button
-                disabled={
-                  (!isValid && dirty) ||
-                  (!isValid && !dirty) ||
-                  (Object.keys(touched).length === 0 &&
-                    touched.constructor === Object)
-                }
-                type="submit"
-                onClick={handleSubmit}
-              >
-                Зареєструватися
-              </button>
+            <button
+              disabled={
+                (!isValid && dirty) ||
+                (!isValid && !dirty) ||
+                (Object.keys(touched).length === 0 &&
+                  touched.constructor === Object)
+              }
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Зареєструватися
+            </button>
 
-              <p>
-                <a href="./">Реєстрація</a>
-              </p>
-            </form>
-          </div>
-        )}
-      </Formik>
-      <p>
-        Книги — это корабли мысли, странствующие по волнам времени и бережно
-        несущие свой драгоценный груз от поколения к поколению.
-      </p>
-      <p>Бэкон Ф.</p>
-    </>
+            <p>
+              Вже з нами? <a href="./">Увійти</a>
+            </p>
+          </form>
+        </div>
+      )}
+    </Formik>
   );
 };
 export default LoginPage;
