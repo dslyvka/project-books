7ebe792947a316724e91f7b0e9194c4ad5047afc
IhorShvydkyi <igorshvydky35@gmail.com>
2022-06-09 21:12:19 +0300

    add styles for books list

---
+++ src/components/Training/TrainingListItem/TrainingListItem.jsx
@@ -0,0 +1,7 @@
+import { ReactComponent as BookIcon } from '../../../images/icons/training/bookIcon.svg';
+
+const TrainingListItem = () => {
+  return <li></li>;
+};
+
+export default TrainingListItem;
--- src/redux/books/books-operations.js
+++ src/redux/books/books-operations.js
@@ -1,28 +1,28 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://project-books-api.herokuapp.com/api';
+// axios.defaults.baseURL = 'https://project-books-api.herokuapp.com/api';
 
-export const fetchBooks = createAsyncThunk(
-  'books/fetchBooks',
-  async (_, rejectWithValue) => {
-    try {
-      const { data } = await axios.get('/books');
+// export const fetchBooks = createAsyncThunk(
+//   'books/fetchBooks',
+//   async (_, rejectWithValue) => {
+//     try {
+//       const { data } = await axios.get('/books');
 
-      return data;
-    } catch (error) {
-      return rejectWithValue(alert(error.message));
-    }
-  },
-);
+//       return data;
+//     } catch (error) {
+//       return rejectWithValue(alert(error.message));
+//     }
+//   },
+// );
 
-export const addBooks = createAsyncThunk('/books/addBooks', async text => {
-  const books = {
-    title: text.title,
-    autor: text.author,
-    year: text.year,
-    pages: text.pages,
-  };
-  const { data } = await axios.post('/books', books);
-  return data;
-});
+// export const addBooks = createAsyncThunk('/books/addBooks', async text => {
+//   const books = {
+//     title: text.title,
+//     autor: text.author,
+//     year: text.year,
+//     pages: text.pages,
+//   };
+//   const { data } = await axios.post('/books', books);
+//   return data;
+// });
