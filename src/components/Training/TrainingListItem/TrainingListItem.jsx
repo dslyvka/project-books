873006f68b7a720ed615a73276f946e8a23c706f
IhorShvydkyi <igorshvydky35@gmail.com>
2022-06-09 21:54:23 +0300

    add styles for full books list and delete button

--- src/components/Training/TrainingListItem/TrainingListItem.jsx
+++ src/components/Training/TrainingListItem/TrainingListItem.jsx
@@ -1,7 +1,30 @@
 import { ReactComponent as BookIcon } from '../../../images/icons/training/bookIcon.svg';
+import { ReactComponent as DeleteIcon } from '../../../images/icons/training/deleteIcon.svg';
+
+import {
+  TrainingListItemStyled,
+  BookIconStyled,
+  TrainingListItemTitle,
+  TrainingListItemAuthor,
+  TrainingListItemYear,
+  TrainingListItemPages,
+  DeleteIconStyled,
+  DeleteButton,
+} from './TrainingListItem.styled';
 
 const TrainingListItem = () => {
-  return <li></li>;
+  return (
+    <TrainingListItemStyled>
+      <BookIconStyled />
+      <TrainingListItemTitle>...</TrainingListItemTitle>
+      <TrainingListItemAuthor>...</TrainingListItemAuthor>
+      <TrainingListItemYear>...</TrainingListItemYear>
+      <TrainingListItemPages>...</TrainingListItemPages>
+      <DeleteButton>
+        <DeleteIconStyled />
+      </DeleteButton>
+    </TrainingListItemStyled>
+  );
 };
 
 export default TrainingListItem;
