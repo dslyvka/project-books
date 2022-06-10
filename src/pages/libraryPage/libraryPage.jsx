15a8bf0fed50708a096bc238d16c5a8fd2934f6c
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-06-10 23:20:02 +0300

    modal
    modal will shown if books arr = 0

--- src/pages/libraryPage/libraryPage.jsx
+++ src/pages/libraryPage/libraryPage.jsx
@@ -1,15 +1,17 @@
 import LibraryModal from '../../components/LibraryModal/LibraryModal';
 import LibraryList from '../../components/LibraryList/LibraryList';
 import LibraryForm from '../../components/LibraryForm/LibraryForm';
+import { useSelector } from 'react-redux';
+
 const LibraryPage = () => {
+  const { items } = useSelector(state => state.books);
   return (
     <div>
       <LibraryForm />
-      {/* <LibraryModal /> */}
+      {items.length === 0 && <LibraryModal />}
       <LibraryList />
     </div>
   );
-  // return null;
 };
 
 export default LibraryPage;
