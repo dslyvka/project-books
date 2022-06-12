d9c9726d7e225aa97afc576be93448934e3d8ccd
dslyvka <daniilslyvka@gmail.com>
2022-06-12 18:36:01 +0300

    fixed register styles

--- src/components/RegisterForm/RegisterForm.styled.js
+++ src/components/RegisterForm/RegisterForm.styled.js
@@ -51,13 +51,19 @@ export const StyledForm = styled.form`
     }
 
     li {
-      margin: 0 auto;
+      margin: 0 auto 20px;
       width: 280px;
 
       position: relative;
 
       @media screen and (min-width: ${breakpoints[1]}px) {
         width: 320px;
+      }
+
+      :last-of-type {
+        @media screen and (min-width: ${breakpoints[1]}px) {
+          margin-bottom: 35px;
+        }
       }
     }
 
@@ -96,9 +102,8 @@ export const StyledForm = styled.form`
       -moz-box-shadow: none;
       box-shadow: none;
 
-      margin: 0 auto 20px;
+      margin: 0 auto;
       padding: 12px 0px 13px 12px;
-      
 
       background-color: #f5f7fa;
       box-shadow: inset 0px 1px 2px rgba(29, 29, 27, 0.15);
@@ -120,13 +125,15 @@ export const StyledForm = styled.form`
     }
 
     .input__error {
-      // position: absolute;
-      // left: 0;
-      // bottom: 0;
       color: red;
-      // margin: 0 auto 5px;
-      padding-bottom: 15px;
+      padding-top: 7px;
       display: block;
+
+      font-family: 'Montserrat';
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 17px;
     }
   }
 
