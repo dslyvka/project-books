d5fabda2665dbcf11c0168cbedecd5fb9b08eb77
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2022-06-16 20:26:28 +0300

    Update Resume
    redirect + animation

--- src/pages/ResumePage/ResumePage.js
+++ src/pages/ResumePage/ResumePage.js
@@ -1,11 +1,26 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { PageAnimation } from '../../components/PageAnimation/PageAnimation';
 import ResumeList from '../../components/ResumeList/ResumeList';
 import { Div } from './ResumePage.styled';
 
 const ResumePage = () => {
+  const { isLoggedIn } = useSelector(state => state.auth);
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    !isLoggedIn && navigate((location.pathname = '/library'));
+  });
+
   return (
-    <Div>
-      <ResumeList />
-    </Div>
+    isLoggedIn && (
+      <PageAnimation>
+        <Div>
+          <ResumeList />
+        </Div>
+      </PageAnimation>
+    )
   );
 };
 
