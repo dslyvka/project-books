2fbf041c6ac5c0ce1e8bb45d44d06c780a67a88c
dslyvka <daniilslyvka@gmail.com>
2022-06-18 16:14:48 +0300

    fixes

--- src/pages/TrainingPage/TrainingPage.js
+++ src/pages/TrainingPage/TrainingPage.js
@@ -2,24 +2,35 @@ import Container from '../../components/Container/Container';
 import TrainingWrapper from '../../components/Training/TraningWrapper';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import trainingActions from '../../redux/training/trainingActions';
+import actions from '../../redux/auth/auth-actions';
 
 import { PageAnimation } from '../../components/PageAnimation/PageAnimation';
 
 import StartTrainingBtn from '../../components/StartTrainingBtn/StartTrainingBtn';
 
 const TrainingPage = () => {
-  const { isLoggedIn } = useSelector(state => state.auth);
+  // const { isLoggedIn } = useSelector(state => state.auth);
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
   const location = useLocation();
 
+  const { isLoggedIn, token } = useSelector(state => state.auth);
+
+  useLayoutEffect(() => {
+    if (!isLoggedIn && token) {
+      dispatch(actions.fetchCurrentUser());
+    }
+  }, [dispatch, isLoggedIn, token]);
+
+  // useEffect(() => {
+  //   !isLoggedIn && navigate((location.pathname = '/login'));
+  // });
+
   useEffect(() => {
-    !isLoggedIn && navigate((location.pathname = '/login'));
-  });
-  useEffect(() => {
+    // dispatch(actions.fetchCurrentUser())
     dispatch(trainingActions.getCurrTraining());
   }, [dispatch]);
 
